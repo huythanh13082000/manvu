fd8b5e86a4b6ffb9266c416fafc00d7d2bec840c
HuyThanh <huythanh1308bn@gmail.com>
2022-08-05 10:34:40 +0700

    update ui 05/08

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -5,22 +5,35 @@ import CardMedia from '@mui/material/CardMedia'
 import {Button, CardActionArea, Grid} from '@mui/material'
 import './card.css'
 
-export default function MultiActionAreaCard(props = {}) {
+interface Props {
+  width?: string
+  height?: string
+  flag?: boolean
+}
+
+export default function CardBase(props: Props = {}) {
   return (
     <>
-      <Grid style={{position: 'relative', width: '315px'}}>
+      <Grid
+        style={{
+          position: 'relative',
+          width: props.width || '315px',
+          margin: '0 1rem',
+        }}
+      >
         {/* <Loading /> */}
         {/* <CircularProgress
           style={{position: 'absolute', top: '45%', left: '41%'}}
         /> */}
         <Card
-          sx={{maxWidth: 317, height: '466px'}}
+          sx={{maxWidth: 317, height: props.height || '466px'}}
           style={{marginTop: '2rem', paddingBottom: '1rem'}}
         >
           <CardActionArea style={{fontFamily: 'Noto Sans KR'}}>
             <CardMedia
               component='img'
-              height='317'
+              height={props.width}
+              width={props.width}
               image='/img/img3.png'
               alt='green iguana'
             />
@@ -41,37 +54,37 @@ export default function MultiActionAreaCard(props = {}) {
                 alt='heart'
               />
             </Grid>
-
-            <Grid
-              style={{
-                position: 'absolute',
-                top: '0rem',
-                left: '1rem',
-              }}
-            >
-              <Grid position='relative'>
-                <img src='/img/flag.png' alt='flag' />
-                <span className='c-span1'>1</span>
+            {props.flag ? (
+              <Grid
+                style={{
+                  position: 'absolute',
+                  top: '0rem',
+                  left: '1rem',
+                }}
+              >
+                <Grid position='relative'>
+                  <img src='/img/flag.png' alt='flag' />
+                  <span className='c-span1'>1</span>
+                </Grid>
               </Grid>
-            </Grid>
+            ) : null}
+
             <CardContent>
               <Grid container style={{padding: '5px'}}>
                 <Grid item xs={12} container style={{fontWeight: 'bold'}}>
-                  <Grid item xs={5} className='c-p1'>
+                  <Grid className='c-p1'>
                     <Grid container>
-                      <Grid marginRight='5px'>
-                        <img src='/img/N.png' style={{width: '20px'}} alt='N' />
+                      <Grid>
+                        <img
+                          src='/img/N.png'
+                          style={{width: '20px', marginRight: '5px'}}
+                          alt='N'
+                        />
                       </Grid>
                       <Grid>Blog Naver</Grid>
                     </Grid>
                   </Grid>
-                  <Grid
-                    style={{
-                      border: '1px solid #E1E1E1',
-                      height: '20px',
-                      marginRight: '15px',
-                    }}
-                  ></Grid>
+                  <Grid style={{padding: '0 5px'}}>|</Grid>
                   <Grid item xs={6} className='c-p2'>
                     2일후에 시작합니다
                   </Grid>
@@ -84,7 +97,12 @@ export default function MultiActionAreaCard(props = {}) {
                 <Grid item xs={12}>
                   <span>주소</span>
                 </Grid>
-                <Grid container xs={12} style={{alignItems: 'center'}}>
+                <Grid
+                  container
+                  xs={12}
+                  style={{alignItems: 'center'}}
+                  marginBottom='1rem'
+                >
                   <Grid className='c-p5 ' marginRight='10px'>
                     신청100/12
                   </Grid>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,12 @@
-import {Grid} from '@mui/material'
+import {Button, Grid} from '@mui/material'
 import React from 'react'
 import Footer from '../../components/footer'
 import Header from '../../components/header'
 import EastIcon from '@mui/icons-material/East'
 import './home.css'
+import CardBase from '../../components/card'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import CardCampaign from '../../components/CardCampaign'
 
 const Home = () => {
   return (
@@ -48,6 +51,138 @@ const Home = () => {
             모두 보기 <EastIcon />
           </p>
         </Grid>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        container
+        justifyContent='space-between'
+        padding='0 3rem 2rem 3rem'
+      >
+        <CardBase flag />
+        <CardBase flag />
+        <CardBase flag />
+        <CardBase flag />
+        <CardBase flag />
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        container
+        justifyContent='space-between'
+        padding='0 3rem 2rem 3rem'
+      >
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        {/* <CardBase width='249px' height='423px' /> */}
+      </Grid>
+      <Grid item xs={12} padding='0 3rem 2rem 3rem'>
+        <Button className='h-load-more' variant='outlined'>
+          더 보기 <ExpandMoreIcon />
+        </Button>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        padding='4rem 3rem'
+        bgcolor=' #D1D1D1'
+        container
+        justifyContent='space-between'
+      >
+        <Grid width='317px' margin='1rem'>
+          <p className='h-p4'>120+</p>
+          <p className='h-p5'>캠페인 진행예정</p>
+          <p className='h-p6'>캠페인 진행예정을 설명하기</p>
+          <Button variant='contained' disableElevation className='h-button'>
+            모두 보기
+          </Button>
+        </Grid>
+
+        <Grid margin='1rem'>
+          <CardCampaign />
+        </Grid>
+        <Grid margin='1rem'>
+          <CardCampaign />
+        </Grid>
+        <Grid margin='1rem'>
+          <CardCampaign />
+        </Grid>
+        <Grid margin='1rem'>
+          <CardCampaign />
+        </Grid>
+        <Grid margin='1rem'>
+          <CardCampaign />
+        </Grid>
+        <Grid margin='1rem'>
+          <CardCampaign />
+        </Grid>
+        <Grid margin='1rem'>
+          <CardCampaign />
+        </Grid>
+        <Grid margin='1rem'>
+          <CardCampaign />
+        </Grid>
+        <Grid margin='1rem'>
+          <CardCampaign />
+        </Grid>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        container
+        padding='0 3rem'
+        alignItems='center'
+        justifyContent='space-between'
+      >
+        <Grid>
+          <p className='h-p1'>추전캠페인</p>
+          <p className='h-p2'>추전캠페인을 설명하기</p>
+        </Grid>
+        <Grid>
+          <p className='h-p3'>
+            모두 보기 <EastIcon />
+          </p>
+        </Grid>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        container
+        justifyContent='space-between'
+        padding='0 3rem 2rem 3rem'
+      >
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        <CardBase width='255px' height='423px' />
+        {/* <CardBase width='249px' height='423px' /> */}
+      </Grid>
+      <Grid item xs={12} padding='0 3rem 2rem 3rem'>
+        <Button className='h-load-more' variant='outlined'>
+          더 보기 <ExpandMoreIcon />
+        </Button>
       </Grid>
 
       <Grid item xs={12}>
