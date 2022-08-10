b55d36955a153ffd8579e465f5919c108e302029
HuyThanh <huythanh1308bn@gmail.com>
2022-08-10 18:14:37 +0700

    update 10/8

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -20,6 +20,9 @@ export default function Header() {
     // navigate('/login')
     // console.log(user)
   }
+  React.useEffect(()=>{
+    
+  },[])
   const handleRegister = () => {
     navigate('/termsofuse')
   }
